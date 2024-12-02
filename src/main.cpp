b5d47a01a2d163b5c076628df74759d0ee8e51ed
Shashank Sapre <shashank.sapre@obconnect.io>
2024-12-02 13:01:40 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
   std::cout << "$ ";
-  
-  std::string input;
-  std::getline(std::cin, input);
 
-  std::cout << input << ": command not found\n";
+  bool isExit = false;
+
+  while (isExit == false)
+  {
+    std::string input;
+    std::getline(std::cin, input);
+    if (input.substr(0, 4) == "exit")
+    {
+      isExit = true;
+      continue;
+    }
+    else
+    {
+      std::cout << input << ": command not found\n";
+      std::cout << "$ ";
+    }
+  }
 }
